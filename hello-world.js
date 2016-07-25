7443d65874f5dfcda6701f054a01cecf332085b5
AtomikBioHazard <atomikbiohazard@gmail.com>
2016-07-25 17:38:32 -0500

    Add files via upload

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+/* *** Various ways to print Hello World *** */
+
+console.log("Hello World");
+/* ***** ***** ***** ***** ***** */
+
+document.querySelector("body").innerHTML = "Hello World";
+
+/* ***** ***** ***** ***** ***** */
+
+var message = "Hello World";
+
+console.log(message);
